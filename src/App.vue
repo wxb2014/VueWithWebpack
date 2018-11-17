46aa1308aad9896cb20f5a6acbff9d233c75a05e
wxb <328987129@qq.com>
2018-11-17 13:59:03 +0800

    webpac + vue 单页面应用

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <p class="test">vue-cli-test  vue-cli-test  vue-cli-test  </p>
+        <p class="test">{{msg}}</p>
+    </div>
+</template>
+<script>
+    export default {
+        name:'App',
+        data:function(){
+            return {
+                msg:"hello vue1 !!"
+            }
+        },
+    }
+</script>
+<style >
+    .test{
+        color:#020202;
+        font-size:18px;
+    }
+</style>
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import App from "./App"
+import Vue from "vue"
+
+new Vue({
+    el:"#app",
+    template:'<App/>',  //模板使用标签<app/>
+    components:{App}   //组件app
+})
